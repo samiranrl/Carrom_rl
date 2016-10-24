dc0925138672ee2571018684833943f5c94af908
Samiran Roy <samiranrl@users.noreply.github.com>
2016-10-24 15:47:46 +0530

    Update generate_stats.py

--- logs/generate_stats.py
+++ logs/generate_stats.py
@@ -19,7 +19,7 @@ try:
                          [1].split(" turns")[0])
             time += float(sample.split("Realtime taken: ")[1].split(" s")[0])
 
-    print "Average turn to clear the board: ", turns/experiments
+    print "Average turn to clear the board: ", turns*1.0/experiments
     print "Total time taken: ", time
     print "Number of Experiments: ", experiments
     if flag == 0:
